539c399ca12f0b6f68c099322ed780ab5ba38c9e
Souptik Taran <mr.souptiktaran@gmail.com>
2024-11-08 20:43:47 +0530

    Menu Page with bottom nav bar

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,54 @@
+import { View, Image, Text, StyleSheet , Pressable } from "react-native";
+import Colors from "../constants/Colors";
+import { Product } from '@/src/types/types';
+import { Link } from "expo-router";
+
+export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+
+
+
+type ProductListItemProps = {
+    product: Product
+}
+
+
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+    return (
+        <Link href={`/menu/${product.id}`} asChild>
+            <Pressable style={styles.container}>
+                <Image source={{ uri: product.image || defaultPizzaImage }}
+                    style={styles.image}
+                    resizeMode="contain"
+                />
+                <Text style={styles.title}>{product.name}</Text>
+                <Text style={styles.price}>${product.price}</Text>
+            </Pressable>
+        </Link>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius: 10,
+        flex: 1,
+        maxWidth: '50%',
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 1,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: '600',
+        marginVertical: 10,
+    },
+    price: {
+        color: Colors.light.tint,
+        fontWeight: 'bold',
+    }
+});
+
+export default ProductListItem
